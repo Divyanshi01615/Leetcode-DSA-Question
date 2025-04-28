9a1fc12f5d6c2012b1c0818c7d9dc636e4721719
Divyanshi Singh Bhadauriya <105812747+Divyanshi01615@users.noreply.github.com>
2025-04-28 23:45:37 +0530

    Time: 15 ms (43.08%), Space: 18.5 MB (12.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,16 +1,6 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        s1=list(s)
-        t1=list(t)
-        s1.sort()
-        t1.sort()
-        for i,j in zip(s1,t1):
-            if s1!=t1:
-                return False
-        return True
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        return sorted(s)==sorted(t)
         
